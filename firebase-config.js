f73b76164f9b1a3200f8670a9e6b66ead16b3c66
chandana377 <vn377@nau.edu>
2025-11-29 18:14:03 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,22 @@
-// --- Firebase Core Imports ---
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
-// --- Firebase AUTH ---
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// --- Firebase FIRESTORE ---
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
-// --- Your Firebase Config ---
+// Your config
 const firebaseConfig = {
   apiKey: "AIzaSyCHejhJGn5FG-Cq0towJkhyv2BduzPjIAQ",
   authDomain: "campus-cart-943c1.firebaseapp.com",
   projectId: "campus-cart-943c1",
-  storageBucket: "campus-cart-943c1.firebasestorage.app",
+  storageBucket: "campus-cart-943c1.appspot.com",
   messagingSenderId: "276374239028",
   appId: "1:276374239028:web:92c76d2b0158d07a6c1761"
 };
 
-// --- Initialize Firebase ---
-const app = initializeApp(firebaseConfig);
-
-// Export Firebase services so other .js files can use them
+// INIT
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
